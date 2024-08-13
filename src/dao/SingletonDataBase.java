3b569d4c7c46a3f58817dd47514795481b07a535
Nehemie <64799269+nehemie001@users.noreply.github.com>
2024-08-13 16:15:06 +0000

    update the comments

--- src/dao/SingletonDataBase.java
+++ src/dao/SingletonDataBase.java
@@ -5,18 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SingletonDataBase {
-    //URL de connexion à la base de données etab_db
+//    Connexion in the database etab_db
     private final static String URL="jdbc:mysql://localhost:3306/etab_db";
-    // Nom d'utilisateur
+//    User name
     private final static String USERNAME="root";
-    // Mot de pass
+//    Password
     private final static String PASSWORD="";
-    // Déclaration d'un objet connexion qui provient de java.sql.Connection
+//     Déclaration in the objet connexion who coming de java.sql.Connection
     static Connection connection;
 
     private SingletonDataBase(){
         try  {
-            // Création d'une nouvelle connexion à la base de données
+//            Create a new connexion in the database
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
         } catch (SQLException e) {
